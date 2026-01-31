9403e7fddca52e100abc0e7389ef28e79cb0fd9e
DID <amadou.ben.rahim.camara@gmail.com>
2026-01-31 21:03:59 +0100

    TestD

--- app/Imports/TicketsImport.php
+++ app/Imports/TicketsImport.php
@@ -4,12 +4,13 @@ namespace App\Imports;
 
 use App\Models\Ticket;
 use Maatwebsite\Excel\Concerns\ToModel;
+use Maatwebsite\Excel\Concerns\WithStartRow;
 use Illuminate\Support\Facades\Session;
 use Illuminate\Support\Str;
 use Illuminate\Support\Facades\Auth;
 
 
-class TicketsImport implements ToModel
+class TicketsImport implements ToModel, WithStartRow
 {
     /**
     * @param array $row
@@ -19,6 +20,9 @@ class TicketsImport implements ToModel
     public function model(array $row)
     {
         $user_id = Auth::user()->id;
+        // Check if row is empty or invalid if needed, but for now just map
+        if(!isset($row[1])) return null; 
+
         return new Ticket([
             'user'     => $row[1],
            'password'    => $row[2],
@@ -27,5 +31,13 @@ class TicketsImport implements ToModel
            'tarif_id' => Session::get("tarif_id"),
            'user_id' => $user_id,
         ]);
+    }
+
+    /**
+     * @return int
+     */
+    public function startRow(): int
+    {
+        return 2;
     }
 }
