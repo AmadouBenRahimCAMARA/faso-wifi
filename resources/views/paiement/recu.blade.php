9c9df271c64cf477f4a73f934f782c84ec731aea
DID <amadou.ben.rahim.camara@gmail.com>
2026-01-31 14:22:28 +0100

    Test4

--- resources/views/paiement/recu.blade.php
+++ resources/views/paiement/recu.blade.php
@@ -157,7 +157,7 @@
                 </div>
                 <div class="column postion-relative">
                     <p class="text-center text-uppercase postion-absolute-top1 pt-2 w-100">
-                        <img width="120px" src="../../../public/assets/img/logo.png" alt="LOGO WiLink Tickets" /> <br />
+                        <img width="120px" src="{{ asset('assets/img/logo.png') }}" alt="LOGO WiLink Tickets" /> <br />
                         WiLink Tickets<br />
                         <span class="text-uppercase">+226 54 78 19 78 / +226 56 36 80 34 / +226 65 86 33 36 </span><br />
                         <span class="text-uppercase">info@wilinktickets.com </span><br />
